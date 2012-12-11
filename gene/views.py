9d98ccd99d7e276a85552a4fb8c06062f132b984
kirangilvaz <kirangilvaz@gmail.com>
2012-12-11 11:40:13 -0800

    updated

--- gene/views.py
+++ gene/views.py
@@ -1,28 +1,28 @@
 from django.http import HttpResponse,HttpResponseRedirect
 import sys
 from django.shortcuts import render_to_response
-#from gene.models import *
 from django.template import Context, loader
 from django.core.context_processors import csrf
 from utility import handle_uploaded_file
 sys.stdout=sys.stderr
 
+#render template to upload data
 def upload(request):
+	#update value to prevent CSRF attacks
 	c={}
 	c.update(csrf(request))
 	varData="Kiran"
 	template=loader.get_template('../templates/upload.html')
-	#context=Context({'varData':varData})
-	#return HttpResponse(template.render(context))
 	return render_to_response('../templates/upload.html',c)
 
 def index(request):
 	return HttpResponse('Hello Universe!')
 
+#view to handle uploaded data
 def FileUpload(request):
 	if request.method=='POST':
 		fileObj=request.FILES['file']
 		email=str(request.POST.get('email'))
 		handle_uploaded_file(fileObj,email)
 		return HttpResponse("You file has been uploaded. Your data will be emailed to you at "+email+" as soon as we have processed your request.")
-	return HttpResponse('Sorry. Something went wrong')
+	return HttpResponse('Something went wrong. Please go back and try again')
